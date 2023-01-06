4a8a6a4c3e289a4e0a8a83cc2fc5b1ef9e2412a2
disemio <33365949+djmalibiran@users.noreply.github.com>
2023-01-06 21:06:36 +0800

    Update hero

--- index.php
+++ index.php
@@ -4,11 +4,21 @@ get_header();
 
 if( get_field('hero_background_image') ): ?>
     <?php $image = get_field('hero_background_image'); ?>
-    <div id="hero" class="container-fluid px-0 py-5 py-md-64 py-lg-96 text-white" style="background-image: url(<?php echo $image['url']; ?>);">
-        <?php  if( get_field('hero_text') ): ?>
-            <h1 class="container"><?php the_field('hero_text'); ?></h1>
-            <h1 class="container"><?php the_field('hero_button'); ?></h1>
-        <?php endif; ?>
+    <div id="hero" class="container-fluid px-0 py-5 py-md-64 py-lg-96 text-white" style="background-image: url(<?php echo $image['url']; ?>); background-size: cover; background-position: center;">
+        <div class="container">
+            <?php  if( get_field('hero_text') ): ?>
+                <h1><?php the_field('hero_text'); ?></h1>
+                <?php 
+                    $hero_btn = get_field('hero_button');
+                    if( $hero_btn ): 
+                        $hero_btn_url = $hero_btn['url'];
+                        $hero_btn_title = $hero_btn['title'];
+                        $hero_btn_target = $hero_btn['target'] ? $hero_btn['target'] : '_self';
+                        ?>
+                    <a class="btn btn-primary position-relative z-1" href="<?php echo esc_url( $hero_btn_url ); ?>" target="<?php echo esc_attr( $hero_btn_target ); ?>"><?php echo esc_html( $hero_btn_title ); ?></a>
+                <?php endif; ?>
+            <?php endif; ?>
+        </div>
     </div>
 <?php endif; ?>
 
